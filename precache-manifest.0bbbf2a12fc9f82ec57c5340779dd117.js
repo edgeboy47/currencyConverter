4ff8f610968a195449a183821803153e57fbdaee
edgeboy47 <dmills1994@gmail.com>
2019-02-01 01:00:19 -0400

    Updates

---
+++ precache-manifest.0bbbf2a12fc9f82ec57c5340779dd117.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87c191006a7fd8d9a0a1",
+    "url": "/currencyConverter/static/js/runtime~main.87c19100.js"
+  },
+  {
+    "revision": "460013dcf6edfc1b5168",
+    "url": "/currencyConverter/static/js/main.460013dc.chunk.js"
+  },
+  {
+    "revision": "5a5320ec7f56af4a8507",
+    "url": "/currencyConverter/static/js/1.5a5320ec.chunk.js"
+  },
+  {
+    "revision": "460013dcf6edfc1b5168",
+    "url": "/currencyConverter/static/css/main.2f688af6.chunk.css"
+  },
+  {
+    "revision": "7b8ead8be060edb275406a050402b117",
+    "url": "/currencyConverter/index.html"
+  }
+];
